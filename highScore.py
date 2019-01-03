9100e4f7db2faa1ca06328568da411c934a7a669
Brody Cragg <42553426+brodycragg@users.noreply.github.com>
2019-01-03 13:59:49 -0600

    Added feature to see how much exp til next level

--- highScore.py
+++ highScore.py
@@ -1,15 +1,24 @@
 from argparse import ArgumentParser
 from lxml import html
 import requests
+import csv
+
 
 parser = ArgumentParser(description = "Username to be looked up")
 parser.add_argument("-u",dest="user", required=True, help = "User to look up")
+parser.add_argument("-s",dest="skill", required=True, help = "Skill to train")
 args = parser.parse_args()
 user = args.user
+skill = args.skill.title()
+
+with open('experienceNeeded.csv','r') as file:
+    contents = csv.reader(file)
+    experienceTables = [x for x in contents]
+experienceTables[0][0] = 1
 
 skills = ['Overall','Attack','Defence',
     	  'Strength','Hitpoints','Ranged','Prayer','Magic','Cooking',
-	      'Woodcutting','Fletching','Fishing','Firemaking''Crafting',
+	      'Woodcutting','Fletching','Fishing','Firemaking','Crafting',
 	      'Smithing','Mining' ,'Herblore','Agility','Thieving','Slayer','Farming',
 	      'Runecraft', 'Hunter','Construction']
 
@@ -19,13 +28,27 @@ def lookup(name):
     page = requests.get(url)
     tree = html.fromstring(page.content)
     exp = tree.xpath('//body//text()')[0]
-    output,result,final = [],[],[]
+    output,result,experience,final = [],[],[],[]
     [output.append(i) for i in exp.splitlines()]
     for i in range(0, len(output)-1):
         result.append([x.strip() for x in output[i].split(',')])
         final.append(result[i][1])
 
     levels = dict(zip(skills,final))
-    print(levels)
+    # print(levels)
 
+    for i in range(0,24):
+        experience.append(result[i][2])
+
+    experience = dict(zip(skills,experience))
+    experienceInLevel = int(experience[skill])
+
+    for i in range(0,len(experienceTables)):
+        if experienceInLevel > int(experienceTables[i][1].replace(',','')):
+            level = experienceTables[i][0]
+            targetLevel = int(level) + 1
+            targetExp = experienceTables[i+1][1].replace(',','')
+            experienceNeeded = int(targetExp) - int(experienceInLevel)
+
+    print('You are training', skill, 'and need',experienceNeeded, 'exp until you reach level ', targetLevel)
 lookup(user)
